fd757ed42eaa3414050394a51d868cdde34e6eea
AlanAlc <48273314+AlanAlc@users.noreply.github.com>
2022-05-04 14:30:58 -0500

    Commit de Archivos para examen

---
+++ Ejercicios/Ejercicios/kfb.js
@@ -0,0 +1,12 @@
+    // Your web app's Firebase configuration
+    const firebaseConfig = {
+      apiKey: "AIzaSyAR1Dzx0eDzQ6NC37yL2qyFcGcUZky2IAY",
+      authDomain: "freelancer-f3017.firebaseapp.com",
+      databaseURL: "https://freelancer-f3017-default-rtdb.firebaseio.com",
+      projectId: "freelancer-f3017",
+      storageBucket: "freelancer-f3017.appspot.com",
+      messagingSenderId: "168942475452",
+      appId: "1:168942475452:web:84d2794c8b5cb776c118b9"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
